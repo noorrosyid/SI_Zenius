2c977ae4f810a2f85832e82030418b9d3558b623
Muhammad Noorrosyid Sulaksono <103987120+noorrosyid@users.noreply.github.com>
2022-05-17 08:23:21 +0700

    Add files via upload
    Loops and Function Assignment Studi Independen Zenius

---
+++ Leap_Year_Function.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day4HW_Muhammad Noorrosyid Sulaksono  
+
+# Create a function called ‘leap_year’ that takes in an integer value (let’s call it x). 
+# 
+# Your function should:
+# 
+# - Check if the input integer(x) is a leap year or not. 
+# - If the input integer is a leap year, please print out (‘Year x is a leap year’)
+# - If the input integer is NOT a leap year:
+# - Find out the next leap year after year x, and please print out (‘Year x is not a leap year. The next leap year after x is <give your answer here>’)
+
+# In[3]:
+
+
+#function to input the year
+def input_leap_year(x): 
+    return x
+
+#function to calculate the right leap year
+def true_leap_year(x): 
+    return(int(4-(float(x)%4)+float(x)))
+
+#function to check if the input is a leap year or not, if the input is not a leap year it will be printed the next leap year after the year's input.
+def leap_year(x):
+    if float(x)%4 == 0:
+        print('Year', input_leap_year(x), 'is a leap year.')
+    else:
+        print('Year', input_leap_year(x), 'is not a leap year. The next leap year after', input_leap_year(x), 'is' , true_leap_year(x))
+
+
+# In[4]:
+
+
+leap_year(2001)
+
+
+# In[ ]:
+
+
+
+
